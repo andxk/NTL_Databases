01a7e54526c9e86d2372a07d229823a420cd28c7
andxk <kupex@ya.ru>
2023-07-30 22:08:43 +0300

    HW4.1 Create and Insert

---
+++ HW4/MusicSite0_CR.sql
@@ -0,0 +1,64 @@
+CREATE TABLE IF NOT EXISTS genre (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS singer (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS album (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	album_year int NOT NULL CHECK (album_year > 1900)
+);
+
+
+CREATE TABLE IF NOT EXISTS track (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	--len int	NOT NULL,
+	len TIME NOT NULL CHECK (len > '00:00:59') CHECK (len < '01:00:00'),
+	album_id SERIAL REFERENCES album(id),
+	UNIQUE ("name", album_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS collection (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) UNIQUE NOT NULL,
+	coll_year int NOT NULL CHECK (coll_year > 1900)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS collection_track (
+	id SERIAL PRIMARY KEY,
+    collection_id SERIAL NOT NULL REFERENCES collection(id),
+    track_id SERIAL NOT NULL REFERENCES track(id),
+    UNIQUE (collection_id, track_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS album_singer (
+	id SERIAL PRIMARY KEY,
+    singer_id SERIAL NOT NULL REFERENCES singer(id),
+    album_id SERIAL NOT NULL REFERENCES album(id),
+    UNIQUE (singer_id, album_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS genre_singer (
+	id SERIAL PRIMARY KEY,
+    singer_id SERIAL NOT NULL REFERENCES singer(id),
+    genre_id SERIAL NOT NULL REFERENCES genre(id),
+    UNIQUE (singer_id, genre_id)
+);
+
+
+
+
+
---
+++ HW4/MusicSite1_INS.sql
@@ -0,0 +1,91 @@
+-- Ќаполнение таблиц музыкального сайта --
+
+INSERT INTO genre ("name") VALUES ('Rock');
+INSERT INTO genre ("name") VALUES ('Pop');
+INSERT INTO genre ("name") VALUES ('R&B');
+
+
+INSERT INTO singer VALUES (1, 'Madonna');
+INSERT INTO singer VALUES (2, 'Patrick Leonard');
+INSERT INTO singer VALUES (3, 'Michael Jackson');
+INSERT INTO singer VALUES (4, 'Beyonce');
+
+INSERT INTO genre_singer (singer_id, genre_id) VALUES (1, 1); 
+INSERT INTO genre_singer (singer_id, genre_id) VALUES (1, 2);
+INSERT INTO genre_singer (singer_id, genre_id) VALUES (2, 1); 
+INSERT INTO genre_singer (singer_id, genre_id) VALUES (3, 1); 
+INSERT INTO genre_singer (singer_id, genre_id) VALUES (3, 3);
+INSERT INTO genre_singer (singer_id, genre_id) VALUES (4, 3);
+
+--SELECT * FROM singer;
+
+-- album 1 --
+INSERT INTO album (id, name, album_year) VALUES (1, 'Like a Prayer', 1989);
+
+INSERT INTO album_singer (singer_id, album_id) VALUES (1, 1);
+INSERT INTO album_singer (singer_id, album_id) VALUES (2, 1);
+
+INSERT INTO track (name, len, album_id)
+VALUES ('My Love Song', '00:04:52', 1);
+
+INSERT INTO track (name, len, album_id)
+VALUES ('Cherish', '00:05:03', 1);
+
+INSERT INTO track (name, len, album_id)
+VALUES ('Act of Contrition', '00:02:19', 1);
+
+-- album 2 --
+INSERT INTO album (id, name, album_year) VALUES (2, 'Madame X', 2020);
+
+INSERT INTO album_singer (singer_id, album_id) VALUES (1, 2);
+
+INSERT INTO track (name, len, album_id) VALUES ('Crave', '00:03:21', 2);
+INSERT INTO track (name, len, album_id) VALUES ('Crazy', '00:04:02', 2);
+
+-----
+
+
+-- album 3 -- Michael Jackson --
+INSERT INTO album (id, name, album_year) VALUES (3, 'Off the Wall', 1979);
+
+INSERT INTO album_singer (singer_id, album_id) VALUES (3, 3);
+
+INSERT INTO track (name, len, album_id) VALUES ('Rock with You', '00:03:39', 3);
+INSERT INTO track (name, len, album_id) VALUES ('Girlfriend', '00:03:04', 3);
+
+
+-- album 4 -- Beyonce --
+INSERT INTO album (id, name, album_year) VALUES (4, 'B day', 2006);
+INSERT INTO album_singer (singer_id, album_id) VALUES (4, 4);
+
+INSERT INTO track (name, len, album_id) VALUES ('Deja Vu', '00:04:00', 4);
+INSERT INTO track (name, len, album_id) VALUES ('My Kitty Kat', '00:03:55', 4);
+
+
+-- Collectioons --
+--SELECT * FROM track t; 
+--SELECT * FROM collection c; 
+
+INSERT INTO collection ("name", coll_year) VALUES ('Rock Collection 15', 2015);
+INSERT INTO collection ("name", coll_year) VALUES ('Collection 18', 2018);
+INSERT INTO collection ("name", coll_year) VALUES ('Super Collection 20', 2020);
+INSERT INTO collection ("name", coll_year) VALUES ('Best Collection 22', 2022);
+
+INSERT INTO collection_track (collection_id, track_id) VALUES (1, 1);
+INSERT INTO collection_track (collection_id, track_id) VALUES (1, 3);
+INSERT INTO collection_track (collection_id, track_id) VALUES (1, 6);
+
+INSERT INTO collection_track (collection_id, track_id) VALUES (2, 2);
+INSERT INTO collection_track (collection_id, track_id) VALUES (2, 7);
+INSERT INTO collection_track (collection_id, track_id) VALUES (2, 9);
+
+INSERT INTO collection_track (collection_id, track_id) VALUES (3, 4);
+INSERT INTO collection_track (collection_id, track_id) VALUES (3, 1);
+INSERT INTO collection_track (collection_id, track_id) VALUES (3, 8);
+
+INSERT INTO collection_track (collection_id, track_id) VALUES (4, 3);
+INSERT INTO collection_track (collection_id, track_id) VALUES (4, 4);
+INSERT INTO collection_track (collection_id, track_id) VALUES (4, 5);
+
+
+
